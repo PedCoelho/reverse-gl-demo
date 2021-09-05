293ec102501caff960efd4af4a054a3d010cfb98
pedro.silva <pedro.silva@mjv.com.br>
2021-09-05 15:34:38 -0300

    minor fix

--- map_token.js
+++ map_token.js
@@ -1,2 +1,1 @@
-// export default "pk.eyJ1IjoicGJ1bm55IiwiYSI6ImNrdDU3d3VldDA1dG4ydnBnaHhldXB6MHgifQ.w0YB9Jkt0qhHx-oxh_e9Jw";
-export default "pk.eyJ1IjoicGJ1bm55IiwiYSI6ImNrdDU2NDFqeTA1aDgydW1objJ3bml5NHMifQ.tHZFpYY__RH1FIGsmZyKKQ"; //public
+export default "pk.eyJ1IjoicGJ1bm55IiwiYSI6ImNrdDU3d3VldDA1dG4ydnBnaHhldXB6MHgifQ.w0YB9Jkt0qhHx-oxh_e9Jw";
